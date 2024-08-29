3857ed6b4fa105321885e3ddf8fa1ea75e9cd44a
Asyari <122692983+asyrianirya@users.noreply.github.com>
2024-08-29 20:17:22 +0700

    Update CreateElement.js

--- Javascript/CreateElement.js
+++ Javascript/CreateElement.js
@@ -1,37 +1,46 @@
-    // example use
-    document.body.appendChild(createEle('div', 'overlay', {
-        position: "relative",
+const backStage = document.body.appendChild(
+    createEle("div", "overlay", {
+        position: "fixed",
         width: "100vw",
         height: "100vh",
         overflow: "hidden",
         backgroundColor: "rgba(0,0,0,0.2)",
+        display: 'flex',
+        justifyContent: 'center'
     })
-    );
+);
 
-    function createEle(type='div', classes = null, styles = null) {
-        const appendStyle = (element, styles) => {
-            if(styles){
-                Object.assign(element.style, styles);
-            }
+backStage.appendChild(
+    createEle("div", "modal", {
+        position: "fixed",
+        width: "80vw",
+        height: "10vh",
+        backgroundColor: "white",
+        top: '10px',
+        border: '1px solid black',
+        borderRadius: '10px'
+    })
+);
+
+function createEle(type = "div", classes = null, styles = null) {
+    const appendStyle = (element, styles) => {
+        if (styles) {
+            Object.assign(element.style, styles);
         }
-        let div;
-        if (classes) {
-            if (Array.isArray(classes)) {
-                classes.forEach((element) => {
-                    div = document.createElement(type);
-                    div.classList.add(element);
-                });
-                appendStyle(element,styles);
-            } else if (!Array.isArray(classes)) {
-                div = document.createElement(type);
-                div.classList.add(classes);
-                appendStyle(div,styles);
-            } else {
-                div = document.createElement(type);
-                appendStyle(div,styles);
-            }
+    };
+
+    const div = document.createElement(type);
+    if (classes) {
+        if (Array.isArray(classes)) {
+            classes.forEach((element) => {
+                div.classList.add(element);
+            });
         } else {
-            div = document.createElement(type);
+            div.classList.add(classes);
         }
-        return div;
     }
+    if (styles) {
+        appendStyle(div, styles);
+    }
+    return div;
+}
