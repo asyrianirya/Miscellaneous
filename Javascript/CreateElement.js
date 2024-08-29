1cb5226da873bc05fb4f87a69d2c7998276319b0
Asyari <122692983+asyrianirya@users.noreply.github.com>
2024-08-29 19:55:02 +0700

    Create CreateElement.js

---
+++ Javascript/CreateElement.js
@@ -0,0 +1,37 @@
+    // example use
+    document.body.appendChild(createEle('div', 'overlay', {
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+        backgroundColor: "rgba(0,0,0,0.2)",
+    })
+    );
+
+    function createEle(type='div', classes = null, styles = null) {
+        const appendStyle = (element, styles) => {
+            if(styles){
+                Object.assign(element.style, styles);
+            }
+        }
+        let div;
+        if (classes) {
+            if (Array.isArray(classes)) {
+                classes.forEach((element) => {
+                    div = document.createElement(type);
+                    div.classList.add(element);
+                    appendStyle(element,styles);
+                });
+            } else if (!Array.isArray(classes)) {
+                div = document.createElement(type);
+                div.classList.add(classes);
+                appendStyle(div,styles);
+            } else {
+                div = document.createElement(type);
+                appendStyle(div,styles);
+            }
+        } else {
+            div = document.createElement(type);
+        }
+        return div;
+    }
